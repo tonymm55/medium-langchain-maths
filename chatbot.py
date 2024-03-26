cfb3019b4387c233291180fb0c0fdd1eb8c17bd0
TonyMM55 <tonymmoran@gmail.com>
2024-03-26 11:55:51 +0000

    add requirements and env file

---
+++ chatbot.py
@@ -0,0 +1,9 @@
+from langchain_openai import OpenAI
+from langchain.chains import LLMMathChain, LLMChain
+from langchain.prompts import PromptTemplate
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.agents.agent_types import AgentType
+from langchain.agents import Tool, initialize_agent
+from dotenv import load_dotenv
+
+load_dotenv()
